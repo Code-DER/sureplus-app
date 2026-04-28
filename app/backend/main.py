bf1e3b1e1a370346417eaefde042502c4f38753b
CODE-Der <xxxcagang@gmail.com>
2026-04-28 15:20:45 +0800

    feat: fetch users

--- app/backend/main.py
+++ app/backend/main.py
@@ -1,7 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from database import supabase
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello sureplus!"}
+
+@app.get("/users")
+async def get_users():
+    response = supabase.table("User").select("*").execute()
+
+    return response.data
