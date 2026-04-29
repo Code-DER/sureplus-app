9f01d8d82b67cfeb26c01c590c6c92428b2bd8a4
CODE-Der <xxxcagang@gmail.com>
2026-04-29 23:57:59 +0800

    feat: added get user by ID endpoint

--- app/backend/api/users.py
+++ app/backend/api/users.py
@@ -1,11 +1,26 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from services import user_service
 from models.user import UserResponse
 from typing import List
+from uuid import UUID
 
 router = APIRouter()
 
+# Endpoint to fetch all users and their details
 @router.get("/", response_model=List[UserResponse])
 async def get_users():
     response = user_service.fetch_all_users()
     return response.data
+
+# Endpoint to fetch a user by their ID
+@router.get("/{user_id}", response_model=UserResponse)
+async def get_user_by_id(user_id: UUID):
+    try:
+        response = user_service.fetch_user_by_id(str(user_id))
+
+        if not response.data:
+            raise HTTPException(status_code=404, detail="User not found!")
+        
+        return response.data
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="User not found!")
--- app/backend/services/user_service.py
+++ app/backend/services/user_service.py
@@ -1,7 +1,9 @@
 from database import supabase
 
+# User Service to fetch all users and their details
 def fetch_all_users():
     return supabase.table("User").select("*").execute()
 
+# User Service to fetch a user by their id
 def fetch_user_by_id(user_id: int):
     return supabase.table("User").select("*").eq("userID", user_id).single().execute()
